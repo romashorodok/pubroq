a6a2a4c8eb95e5f4586848b225b856b14540aa69
romashorodok <108732029+romashorodok@users.noreply.github.com>
2024-08-28 20:52:47 +0300

    ref: remove http/ws

--- python/my_project/demo.py
+++ python/my_project/demo.py
@@ -1,12 +1,7 @@
 import os
 
-from starlette.applications import Starlette
-from starlette.responses import PlainTextResponse, Response
-from starlette.routing import Mount, Route, WebSocketRoute
-from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from starlette.types import Receive, Scope, Send
-from starlette.websockets import WebSocketDisconnect
 
 ROOT = os.path.dirname(__file__)
 STATIC_ROOT = os.environ.get("STATIC_ROOT", os.path.join(ROOT, "htdocs"))
@@ -14,49 +9,6 @@ STATIC_URL = "/"
 LOGS_PATH = os.path.join(STATIC_ROOT, "logs")
 
 templates = Jinja2Templates(directory=os.path.join(ROOT, "templates"))
-
-
-async def homepage(request):
-    """
-    Simple homepage.
-    """
-    await request.send_push_promise("/style.css")
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-async def echo(request):
-    """
-    HTTP echo endpoint.
-    """
-    content = await request.body()
-    media_type = request.headers.get("content-type")
-    return Response(content, media_type=media_type)
-
-
-async def padding(request):
-    """
-    Dynamically generated data, maximum 50MB.
-    """
-    size = min(50000000, request.path_params["size"])
-    return PlainTextResponse("Z" * size)
-
-
-async def ws(websocket):
-    """
-    WebSocket echo endpoint.
-    """
-    if "chat" in websocket.scope["subprotocols"]:
-        subprotocol = "chat"
-    else:
-        subprotocol = None
-    await websocket.accept(subprotocol=subprotocol)
-
-    try:
-        while True:
-            message = await websocket.receive_text()
-            await websocket.send_text(message)
-    except WebSocketDisconnect:
-        pass
 
 
 async def wt(scope: Scope, receive: Receive, send: Send) -> None:
@@ -90,19 +42,8 @@ async def wt(scope: Scope, receive: Receive, send: Send) -> None:
             )
 
 
-starlette = Starlette(
-    routes=[
-        Route("/", homepage),
-        Route("/{size:int}", padding),
-        Route("/echo", echo, methods=["POST"]),
-        WebSocketRoute("/ws", ws),
-        Mount(STATIC_URL, StaticFiles(directory=STATIC_ROOT, html=True)),
-    ]
-)
-
-
 async def app(scope: Scope, receive: Receive, send: Send) -> None:
     if scope["type"] == "webtransport" and scope["path"] == "/wt":
         await wt(scope, receive, send)
     else:
-        await starlette(scope, receive, send)
+        raise RuntimeError("Invalid protocol use webtransport protocol")
